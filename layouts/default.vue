85bae383282017fd2e093ddcc9dcf567b86c996b
Onnion <marcio.hsdl@gmail.com>
2020-03-19 22:35:46 -0300

    master | init nuxt repo

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Nav />
+    <div id="main">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from "~/components/nav/nav";
+
+export default { components: { Nav } };
+</script>
+
+
+<style lang="scss">
+@import "../assets/scss/styles.scss";
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        qgdi
+      </h1>
+      <h2 class="subtitle">
+        QGDI Site
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+})
+</script>
